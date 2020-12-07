807aae1ecedadf0dd606470ecf6cb83e23ce2a63
Sajeel Aalam <imsajeel.dev@gmail.com>
2020-12-07 13:23:50 +0500

    working on users

--- graphql/typeDefs.js
+++ graphql/typeDefs.js
@@ -7,7 +7,24 @@ module.exports = gql`
     createdAt: String!
     username: String!
   }
+  type User {
+    id: ID!
+    username: String!
+    email: String!
+    token: String!
+    createdAt: String!
+  }
+  input RegisterInput {
+    username: String!
+    password: String!
+    confirmPassword: String!
+    email: String!
+    createdAt: String!
+  }
   type Query {
     getPosts: [Post]
+  }
+  type Mutation {
+    register(registerInput: RegisterInput): User!
   }
 `;
